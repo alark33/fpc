f20889c877e32c2d97966855b882dce0acdef842
alark <gitartem97@gmail.com>
2025-09-21 21:43:48 +0300

    Вывод стихотворения по строкам(readln)

--- nldemo.pas
+++ nldemo.pas
@@ -1,8 +1,19 @@
-program square;
-var
-	x: int64;
+program poem;
 begin
-	read(x);
-	x := x*x;
-	writeln(x)
-end. 
+  writeln('Farewell to you, thou sea of blue!');
+  readln();
+  writeln('A last farewell I bid to thee,');
+  readln();
+  writeln('Thou proudly rushing waters few,');
+  readln();
+  writeln('And beauty''s boundless liberty.');
+  readln();
+  writeln('In vain do we with tears complain —');
+  readln();
+  writeln('No more thy waves will lull my pain,');
+  readln();
+  writeln('Nor quench the thirst that burns within...');
+  readln();
+  writeln('I hear no more thy voice again.');
+  readln();
+end.
