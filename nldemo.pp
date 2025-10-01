56b9cc1d10a7e01e70eef5e14382f24965b68e96
alark <gitartem97@gmail.com>
2025-10-01 19:49:48 +0300

    Константы

--- nldemo.pp
+++ nldemo.pp
@@ -1,18 +1,10 @@
-program degree;
-procedure Powers(x: real; var quad, cube, fourth, fifth: real);
-begin
-	quad := x * x;
-	cube := quad * x;
-	fourth := cube * x;
-	fifth := fourth * x
-end;
-
+program Messagen;
+const
+	meesage = 'Hello, world!';
+	count = 20;
 var
-	p, q, r, t: real;
+	i: integer;
 begin
-	Powers(17.5, p, q, r, t);
-	writeln('Квадрат числа:', p:8:2);     // второй аргумент :8:2 означает ширину поля и количество знаков после запятой
-    writeln('Куб числа:', q:8:2);
-    writeln('Четвертая степень:', r:8:2);
-    writeln('Пятая степень:', t:8:2);
+	for i := 1 to count do
+		writeln(meesage);
 end.
